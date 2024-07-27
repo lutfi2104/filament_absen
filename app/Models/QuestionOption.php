c1b944799a272888127a82dde0d8e9c04c59dbe2
lutfi2104 <lutfi.mla@gmail.com>
2024-07-28 00:31:40 +0700

    tahap2

---
+++ app/Models/QuestionOption.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class QuestionOption extends Model
+{
+    use HasFactory;
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+}
