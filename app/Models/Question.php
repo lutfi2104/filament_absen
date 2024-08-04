3e42a424c2780e4b0609492b4f1c30ac75f9ab3f
lutfi2104 <lutfi.mla@gmail.com>
2024-08-04 21:23:29 +0700

    kedua

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Question extends Model
 {
     use HasFactory;
-    public function option(): HasMany
+    protected $fillable = [
+        'question',
+        'explanation',
+    ];
+    public function options(): HasMany
     {
         return $this->hasMany(QuestionOption::class);
     }
--- app/Models/QuestionOption.php
+++ app/Models/QuestionOption.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class QuestionOption extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
     public function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
